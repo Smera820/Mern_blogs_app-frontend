341e39d06f73f6567f3a4a5ea6cd96db609005c6
smerasabu <smerasabu@gmail.com>
2025-02-12 21:25:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import PostDetails from './pages/PostDetails'
+import EditPost from './pages/EditPost'
+import MyBlogs from './pages/MyBlogs'
+import Profile from './pages/Profile'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import CreatePost from './pages/CreatePost'
+import UserContextProvisder from './context/userContext'
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <UserContextProvider></UserContextProvider>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/write' element={<CreatePost />} />
+          <Route path='/Post/post/:id' element={<PostDetails />} />
+          <Route path='/edit/:id' element={<EditPost />} />
+          <Route path='/myblogs/:id' element={<MyBlogs />} />
+          <Route path='/profile/:id' element={<Profile />} />
+
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/context/UserContext.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import axios from 'react'
+import { createContext, useEffect, useState } from 'react'
+import {URL} from '../url'
+
+export const UserContext=createContext({})
+
+
+export default function UserContextProvider({childern}) {
+    const [user,setUser]= useState(null)
+    useEffect(()=>{
+        getUser()
+    },[])
+
+    const getUser=async()=>{
+        try{
+            const res= await axios.get(URL+"/api/auth/refech",{withCredentials:true})
+            setUser(res.data)
+        }
+        catch(err){
+            console.log(err);
+            
+        }
+    }
+    return (
+        <div>
+            <UserContext.Provider value={{user,setUser}}>
+                {childern}
+            </UserContext.Provider>
+        </div>
+    )
+}
